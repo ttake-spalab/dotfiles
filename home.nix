b9c0d906befd348899c86c345048e577ddb0afbe
ttake-spalab <t.take.spalab@gmail.com>
2026-07-25 01:04:30 +0900

    Add uv

--- home.nix
+++ home.nix
@@ -81,8 +81,22 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  programs.neovim = {
-    enable = true;
-  };
+  programs.neovim.enable = true;
   programs.tmux.enable = true;
+  programs.uv = {
+    enable = true;
+    python = {
+      versions = [
+        "3.13"
+        "3.12"
+      ];
+      default = "3.13";
+    };
+    tool = {
+      packages = [
+        "ruff"
+        "ty"
+      ];
+    };
+  };
 }
