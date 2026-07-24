3ff26bf4d24e923a97b7e5297bcf76593b2bb3df
ttake-spalab <t.take.spalab@gmail.com>
2026-07-25 00:44:12 +0900

    Add CLI tools

--- home.nix
+++ home.nix
@@ -34,8 +34,11 @@
     # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
-    jq
     ffmpeg
+    jq
+    mactop
+    sox
+    tree
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
