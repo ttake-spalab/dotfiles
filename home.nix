ab5930b3e4863b06304a5c9eb20b83331d04971a
ttake-spalab <t.take.spalab@gmail.com>
2026-07-25 23:30:47 +0900

    Refactor uv configuration into module

--- home.nix
+++ home.nix
@@ -83,21 +83,4 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-
-  programs.uv = {
-    enable = true;
-    python = {
-      versions = [
-        "3.13"
-        "3.12"
-      ];
-      default = "3.13";
-    };
-    tool = {
-      packages = [
-        "ruff"
-        "ty"
-      ];
-    };
-  };
 }
---
+++ modules/uv/default.nix
@@ -0,0 +1,19 @@
+{ ... };
+{
+  programs.uv = {
+    enable = true;
+    python = {
+      versions = [
+        "3.13"
+        "3.12"
+      ];
+      default = "3.13";
+    };
+    tool = {
+      packages = [
+        "ruff"
+        "ty"
+      ];
+    };
+  };
+}
